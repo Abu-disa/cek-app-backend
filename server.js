6fb521f47285ca5dc6676d18d463f19af0957262
Abu-disa <abudisa49@gmail.com>
2025-01-19 23:53:40 +0700

    p

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const ongkirRoutes = require("./routes/ongkir");
 const resiRoutes = require("./routes/resi");
 const authRoutes = require("./routes/auth");
-const historyRoutes = require("./routes/history");
 
 const app = express();
 app.use(cors());
@@ -14,7 +13,6 @@ app.use(bodyParser.json());
 app.use("/ongkir", ongkirRoutes);
 app.use("/resi", resiRoutes);
 app.use("/auth", authRoutes);
-app.use("/history", historyRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
